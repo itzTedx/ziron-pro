ba8379d8a6555f471e7b86074af62bdc09e4fb9a
melwin <114172287+itzTedx@users.noreply.github.com>
2025-02-14 16:33:09 +0400

    add utility functions, site configuration, and global styles; implement robots and sitemap routes

---
+++ src/types/index.d.ts
@@ -0,0 +1,33 @@
+declare global {
+  interface SiteConfig {
+    name: string
+    shortName: string
+    description: string
+    url: string
+    ogImage: string
+    logo: string
+    links: {
+      linkedin: string
+      facebook: string
+      instagram: string
+    }
+    contact: string
+  }
+
+  interface Service {
+    metadata: ServiceMetadata
+    content: string
+  }
+
+  interface ServiceMetadata {
+    title?: string
+    description?: string
+    image?: string
+    isFeatured?: boolean
+    id?: number
+    slug: string
+  }
+}
+
+// Need to export something to make it a module
+export {}
